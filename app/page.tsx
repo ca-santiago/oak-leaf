66a368dda349083181db74cbc1980c205ef81ba9
Carmen <ca.santiago.me@gmail.com>
2023-11-28 21:15:24 -0600

    feat: ssr manager page

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,11 @@
-"use client";
-import LoginButton from "@/components/login";
-import { useUser } from "@auth0/nextjs-auth0/client";
+import { getSession } from "@auth0/nextjs-auth0";
+import { redirect } from "next/navigation";
 import Link from "next/link";
 
-export default function Home() {
-  const { user, error, isLoading } = useUser();
+export default async function Home() {
+  const session = await getSession();
 
-  // TODO 11/28 casantiago - Render main page loading component
-  if (isLoading) return <div>Loading...</div>;
+  if (!session) return <a href="/api/auth/login">Login</a>;
 
-  if (error) return <div>Error loading, please refresh the pager</div>;
-  if (!user) return <a href="/api/auth/login">Login</a>;
-
-  return (
-    <div>
-      {/* HEADER just for this page */}
-      <div className="p-3 w-full">
-        <Link
-          className="p-2 px-3 text-white bg-blue-500 rounded-md"
-          href="/profile"
-        >
-          See profile
-        </Link>
-      </div>
-      <section>
-        <div>Some header</div>
-      </section>
-    </div>
-  );
+  return redirect("/manage");
 }
