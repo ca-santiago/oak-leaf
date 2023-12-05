3aacabe307123b713ed44059db6ba7e3fde351ab
Carmen Santiago <greminoficial@gmail.com>
2023-12-05 11:45:01 -0600

    feat: profile page

--- app/manager/page.tsx
+++ app/manager/page.tsx
@@ -1,15 +1,21 @@
-// "use client";
-// import { withPageAuthRequired } from "@auth0/nextjs-auth0/client";
-
-// export default withPageAuthRequired(({ user }) => {
-//   return <div>{user.name}</div>;
-// });
-
 import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import Image from "next/image";
+import { getProfileData } from "./getProfileData";
 
 async function ManagerPage() {
   const session = await getSession();
-  return <div>Hello {session!.user.name}</div>;
+  const { name, picture, email, sub } = session!.user;
+  const res = await getProfileData(sub);
+
+  const displayName = res?.preferredName || name || "";
+
+  return (
+    <div>
+      {displayName && <div>Hello {displayName}</div>}
+      <Image src={picture} alt="yo" height={100} width={100} />
+      <a href="/api/auth/logout">Logout</a>
+    </div>
+  );
 }
 
 export default withPageAuthRequired(ManagerPage, { returnTo: "/" });
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import { getSession } from "@auth0/nextjs-auth0";
 import { redirect } from "next/navigation";
-import Link from "next/link";
 
 export default async function Home() {
   const session = await getSession();
 
   if (!session) return <a href="/api/auth/login">Login</a>;
 
-  return redirect("/manage");
+  return redirect("/manager");
 }
