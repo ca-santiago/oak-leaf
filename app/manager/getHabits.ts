36981fc57fa2f180d3c99bede1d6a1ad927c5502
Carmen <ca.santiago.me@gmail.com>
2023-12-08 17:51:58 -0600

    feat: add data range, code tests, performance and core functions

--- app/manager/getHabits.ts
+++ app/manager/getHabits.ts
@@ -1,4 +1,5 @@
 import { API_CONFIG } from "@/services/api";
+import { Habit_Incidences } from "../types";
 
 export const getHabits = async (token: string) => {
   return fetch(`${API_CONFIG.habitsUrl}`, {
@@ -10,4 +11,21 @@ export const getHabits = async (token: string) => {
       revalidate: 1,
     },
   }).then((data) => data.json());
+};
+
+export const getHabitsV2 = async (
+  token: string,
+  yearRange: string
+): Promise<Habit_Incidences[]> => {
+  return fetch(`${API_CONFIG.habitsUrl}/${yearRange}`, {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+    next: {
+      revalidate: 1,
+    },
+  })
+    .then((data) => data.json())
+    .then(({ data }) => data);
 };
--- app/manager/habit.tsx
+++ app/manager/habit.tsx
@@ -1,22 +1,39 @@
 "use client";
-import { Completion, Habit } from "@prisma/client";
 import React from "react";
 import ActivityCalendar, { Activity } from "react-activity-calendar";
 import { Tooltip as ReactTooltip } from "react-tooltip";
 import moment from "moment-timezone";
 import { createCompletion, setCompletion } from "./completions";
-
-interface _Habit extends Habit {
-  completions: Completion[];
-}
+import {
+  Completion,
+  Habit,
+  Habit_Completions,
+  Habit_Incidences,
+} from "../types";
+import {
+  deserializeStringToArray,
+  mergeNewDateRanges,
+  removeDateFromRanges,
+  serializeArrayToString,
+} from "@/helpers/dateRange";
 
 interface HabitsProps {
-  data: _Habit[];
+  data: Habit_Completions[];
+  token: string;
+}
+
+interface HabitsIncidencesProps {
+  data: Habit_Incidences[];
   token: string;
 }
 
 interface HabitDetailsProps {
-  habit: _Habit;
+  habit: Habit_Completions;
+  token: string;
+}
+
+interface HabitIncidenceDetailsProps {
+  habit: Habit_Incidences;
   token: string;
 }
 
@@ -160,5 +177,158 @@ export const Habits = ({ data, token }: HabitsProps) => {
         <HabitDetail token={token} key={item.id} habit={item} />
       ))}
     </div>
+  );
+};
+
+function mapDateRangeStringToArray(dateRange: string): string[] {
+  const [startDate, endDate] = dateRange.split(":");
+  const datesArray = [];
+
+  const currentDate = new Date(startDate);
+  const lastDate = new Date(endDate);
+
+  while (currentDate <= lastDate) {
+    datesArray.push(currentDate.toISOString().split("T")[0]);
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  return datesArray;
+}
+
+function mapDateRangeToArray(startDate: string, endDate: string): string[] {
+  const datesArray = [];
+
+  const currentDate = new Date(startDate);
+  const lastDate = new Date(endDate);
+
+  while (currentDate <= lastDate) {
+    datesArray.push(currentDate.toISOString().split("T")[0]);
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  return datesArray;
+}
+
+function mapDateRangeToActivityArray(
+  startDate: string,
+  endDate: string
+): Activity[] {
+  const datesArray: Activity[] = [];
+
+  const currentDate = new Date(startDate);
+  const lastDate = new Date(endDate);
+
+  while (currentDate <= lastDate) {
+    datesArray.push({
+      count: 1,
+      date: currentDate.toISOString().split("T")[0],
+      level: 1,
+    });
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  return datesArray;
+}
+
+const HabitIncidence = ({ habit, token }: HabitIncidenceDetailsProps) => {
+  const [yearRange, setYearRange] = React.useState(moment().year());
+  const [incidences, setIncidences] = React.useState<string[]>(
+    // Taking first element for now, cuz I know this arr will contain just one element for now
+    deserializeStringToArray(habit.incidences[0].dateRanges || "")
+  );
+
+  const activities: Activity[] = React.useMemo(() => {
+    const dateArr: Activity[][] = incidences.map((i) => {
+      const [start, end] = i.split(":");
+      return mapDateRangeToActivityArray(start, end);
+    });
+
+    const flatten = dateArr.flat();
+
+    const startingActivity: Activity = {
+      count: 0,
+      date: `${yearRange}-01-01`,
+      level: 0,
+    };
+
+    const endActivity: Activity = {
+      count: 0,
+      date: `${yearRange}-12-31`,
+      level: 0,
+    };
+
+    if (flatten.length < 1) {
+      return [startingActivity, endActivity];
+    }
+
+    if (flatten[0].date !== startingActivity.date) {
+      flatten.unshift(startingActivity);
+    }
+
+    if (flatten[flatten.length - 1].date !== endActivity.date) {
+      flatten.push(endActivity);
+    }
+
+    return flatten;
+  }, [incidences, yearRange]);
+
+  const handleActivityClick = (ac: Activity) => {
+    const formatted = moment(ac.date).tz(moment.tz.guess()).format(dateFormat);
+
+    if (ac.count) {
+      setIncidences((prev) => removeDateFromRanges(prev, formatted));
+    } else {
+      setIncidences((prev) => mergeNewDateRanges(prev, formatted));
+    }
+  };
+
+  return (
+    <div className="m-3 px-4 pb-5 border-2 bg-white border-slate-300 rounded-lg w-fit flex flex-col">
+      <h4 className="ml-1 text-slate-700 font-semibold text-lg mt-2">
+        {habit.habitName}
+      </h4>
+      <ReactTooltip id="react-tooltip" />
+      <div className="mt-3 text-slate-500">
+        <div className="w-fit">
+          <ActivityCalendar
+            // hideMonthLabels
+            // showWeekdayLabels
+            hideColorLegend
+            maxLevel={1}
+            renderBlock={(block, activity) =>
+              React.cloneElement(block, {
+                "data-tooltip-id": "react-tooltip",
+                "data-tooltip-html": moment(activity.date).format("MMM Do YY"),
+              })
+            }
+            eventHandlers={{
+              onClick: (e) => handleActivityClick,
+            }}
+            blockSize={16}
+            blockRadius={3}
+            blockMargin={3}
+            weekStart={0}
+            fontSize={14}
+            hideTotalCount
+            colorScheme="light"
+            theme={{
+              light: ["#f0f0f0", "#4A4"],
+              dark: ["#f0f0f0", "#3cc9ae14"],
+            }}
+            data={activities}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const HabitsIncidences = ({ data, token }: HabitsIncidencesProps) => {
+  return (
+    <>
+      {data.map((item) => (
+        <HabitIncidence key={item.id} habit={item} token={token} />
+      ))}
+    </>
   );
 };
