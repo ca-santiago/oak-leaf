866a53f9e7c9413ab49dec905b37be6de04ac780
Carmen Santiago <greminoficial@gmail.com>
2023-12-11 17:14:16 -0600

    fix: api url per env

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,6 @@
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? process.env.API_URL
+    ? process.env.NEXT_PUBLIC_API_URL
     : "http://localhost:3003";
 
 export const API_CONFIG = {
