005f72e671d713513da48ce69f7d7cdfe4c2c63e
Carmen Santiago <greminoficial@gmail.com>
2024-01-02 16:05:24 -0600

    fix: merge conflicts

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -4,21 +4,14 @@ import { NextApiRequest } from "next";
 export const GET = handleAuth({
   async login(req: NextApiRequest, res: any) {
     const params = new URL(req.url || "").searchParams;
-<<<<<<< HEAD
     const inviteCode = params.get("inviteCode");
     const useSignUp = params.get("useSignUp");
-=======
-    const inviteCode = params.get('inviteCode');
->>>>>>> main
 
     try {
       return handleLogin(req, res, {
         authorizationParams: {
           inviteCode: inviteCode || undefined,
-<<<<<<< HEAD
           screen_hint: useSignUp === "true" ? "signup" : "login",
-=======
->>>>>>> main
         },
         returnTo: "/",
       });
