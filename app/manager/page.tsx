191b7ac76df8477ddb595a11b17c4a84cd7a32f1
Carmen Santiago <greminoficial@gmail.com>
2024-08-15 22:13:22 -0600

    fix(habits): layout

--- app/manager/page.tsx
+++ app/manager/page.tsx
@@ -30,11 +30,11 @@ async function ManagerPage() {
         habits={habits}
         token={accessToken!}
       >
-        <div className="grid row-auto grid-cols-8 h-full w-full flex-1 pt-8 px-8 gap-8">
-          <div className="col-span-2 col-start-3">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-8 2xl:grid-cols-12 h-full w-full flex-1 pt-6 lg:pt-8 px-2 lg:px-8 gap-6 md:gap-4 lg:gap-8">
+          <div className="col-span-1 row-span-1 xl:col-start-2 xl:col-span-3 2xl:col-start-4">
             <HabitsDaySchedule />
           </div>
-          <div className="col-span-2 col-start-5">
+          <div className="col-span-1 row-span-1 xl:col-start-5 xl:col-span-3 2xl:col-start-7">
             <h3 className="text-slate-700 font-semibold text-xl text-center">Observa tu progreso</h3>
             <div className="mt-2">
               <HabitsList />
